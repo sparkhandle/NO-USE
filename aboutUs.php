f2d74b71a19057d190f11d0e48d411ad8122e017
sparkhandle <164982340+sparkhandle@users.noreply.github.com>
2024-06-23 11:29:07 +0530

    PHP

---
+++ aboutUs.php
@@ -0,0 +1,84 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+	
+	<!-- HEAD TAG STARTS -->
+
+	<head>
+	
+  		<meta charset="UTF-8">
+  		<meta name="author" content="Joydeep Dev Nath">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="shortcut icon" href="images/favicon.ico">
+	
+		<title>About Us | Project Meteor</title>
+    
+    	<link href="css/main.css" rel="stylesheet">
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400|Raleway:100,300,400,500|Roboto:100,400,500,700" rel="stylesheet">
+    	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    	<script src="js/jquery-3.2.1.min.js"></script>
+    	<script src="js/main.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+    	
+	</head>
+	
+	<!-- HEAD TAG ENDS -->
+	
+	<!-- BODY TAG STARTS -->
+	
+	<body>
+		
+		<?php 
+		
+			if(!isset($_SESSION["username"])) {
+				include("common/headerLoggedOut.php");
+			}
+			else {
+				include("common/headerLoggedIn.php");
+			}
+		
+		?>
+		
+		<div class="spacer">a</div>
+		
+		<div class="col-sm-12 aboutUsWrapper">
+			
+			<div class="headingOne">
+				
+				About Us
+				
+			</div>
+			
+			<div class="para">
+				
+				Company philosophy:
+
+Our mission is to be the leading and most trustworthy travel management company that sets the standard for professionalism, reliability and transparency to its customers.
+
+
+Strengths:
+We have varied experience with global networking travel management companies
+Broad expertise in the local travel market
+Client-first approach to business
+
+Turn around time experts:
+
+Our excellent rapport of over two decades with the Airlines, Hotels, Car Rentals, Visa Providers has given us the confidence to do the best in times of crisis or emergencies and won us the trust of our clients. Robust decision making process owing to us being a single owner agency enables us to “Focus on exceeding service expectation”.
+				
+			</div>
+			
+		</div> <!-- paymentWrapper -->
+	
+	<div class="spacerLarge">.</div> <!-- just a dummy class for creating some space -->
+			
+		<?php include("common/footer.php"); ?>
+				
+	</body>
+	
+	<!-- BODY TAG ENDS -->
+	
+</html>
